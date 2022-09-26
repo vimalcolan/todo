ea8cdc5b068cceceb5f3bc3d4a26b0d77f603dba
vimal <vimal.selvaraj@colanonline.com>
2022-09-26 20:17:50 +0530

    initial changes

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -5,7 +5,7 @@ const Todolist = ({data,handleRead,newList}) => {
     <div>
         {
             <div className='todo-wrapper d-flex w-50 ml-auto justify-content-between align-items-center'>
-                <p className={'todo-name'+ newList.completed?"strike":null}>{data.name}</p>
+                <p className='todo-name'>{data.name}</p>
                 <div className='icons d-flex'>
                     <div className='mx-2' onClick={()=>{handleRead(data)}}>Mark as read</div>
                     <div>Delete</div>
