b2fd7ea1b392ced8823e1c1800c64b40495fa94a
vimal <vimal.selvaraj@colanonline.com>
2022-09-27 16:40:54 +0530

    changes completed

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import Todolist from './components/Todolist';
 function App() {
   const[todo,setTodo]=useState("");
   const[todoList,setTodoList]=useState([]);
+  const[editId,setEditId]=useState("");
+
+  // ---------------------Tracking changes in input field------------------------
   const handleChange=(e)=>{
     setTodo(e.target.value);
   }
+   // ---------------------submitting value(both edit and add)------------------------
   const addTodo=(e)=>{
-    e.preventDefault();
-    setTodoList([...todoList,{name:todo,completed:false}]);
-    console.log(todoList);
-    setTodo("")
+      e.preventDefault();
+      if(editId){
+        const editTodo=todoList.find((i)=>{return(i.name===editId)});
+        const updateTodo=todoList.map((t)=>
+            t.name==editTodo.name?(t={name:todo,completed:false}):{name:t.name,completed:false}
+        );
+        setTodoList(updateTodo);
+        setEditId("");
+        setTodo("");
+        return;
+      }
+    if(todo!==""){
+      setTodoList([...todoList,{name:todo,completed:false}]);
+      console.log(todoList);
+      setTodo("");
+   }
   }
+   // ---------------------Handling done or not------------------------
   const handleRead=(data)=>{
   let newList=  todoList.map((read)=>{
       if(read.name === data.name){
@@ -23,7 +40,23 @@ function App() {
     });
     console.log(newList);
   }
-  
+   // ---------------------Handling delete------------------------
+  const handleDelete=(data)=>{
+   let remainingList=todoList.filter((e)=>{
+   return( e.name!=data.name)
+  });
+   setTodoList(remainingList)
+    }
+     // ---------------------handling edit------------------------
+    const handleEdit=(data)=>{
+      let editItem=todoList.find((e)=>{
+      return( e.name==data.name)
+     });
+      setTodo(editItem.name);
+      setEditId(editItem.name);
+
+       }
+
   return (
     <div className="App">
       <div className='todo-form'>
@@ -31,14 +64,14 @@ function App() {
         <div className='todo-form'>
           <form onSubmit={addTodo}>
             <input type="text" onChange={handleChange} placeholder="Enter todo" value={todo}/>
-            <button type="submit" value="Add Todo" className='btn btn-primary' >ADD Todo</button>
+            <button type="submit" value="Add Todo" className='btn btn-primary' >{editId?"Update todo":"Add Todo"}</button>
           </form>
         </div>
       </div>
      {
       todoList.map((data,index)=>{
         return(
-          <Todolist key={index} data={data} handleRead={handleRead} />
+          <Todolist key={index} data={data} handleRead={handleRead} handleDelete={handleDelete} handleEdit={handleEdit}/>
         )
       })
      }
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Todolist = ({data,handleRead}) => {
+const Todolist = ({data,handleRead,handleDelete,handleEdit}) => {
   return (
     <div>
         {
             <div className='todo-wrapper d-flex w-50 ml-auto justify-content-between align-items-center'>
-                <p className='todo-name'>{data.name}</p>
+                <p className={`todo-name`}>{data.name}</p>
                 <div className='icons d-flex'>
+                  <div onClick={()=>{handleEdit(data)}}>Edit</div>
                     <div className='mx-2' onClick={()=>{handleRead(data)}}>Mark as read</div>
-                    <div>Delete</div>
+                    <div onClick={()=>{handleDelete(data)}}>Delete</div>
                 </div>
             </div>
         }
